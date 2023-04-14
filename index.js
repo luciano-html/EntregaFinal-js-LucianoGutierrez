03ed72074c61b379a60dea8bca996c334a62977e
Luciano Gutierrez <luciano.c.gutierrez@gmail.com>
2023-04-14 02:21:07 -0300

    Se agrego consumo de api

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ class User {
     }
 }
 
-const users = []
+const users = [
+    {
+        name: "papu",
+        lastName: "ome" 
+
+    }
+]
 users.push(new User("luciano", "gutierrez"))
 const btnSwitch = document.getElementById("flexSwitch")
 const span = document.getElementById("span")
@@ -16,6 +22,28 @@ if(localStorage.getItem("login")){
     const spanHeader = document.getElementById("spanHeader")
     spanHeader.innerText = `Sesion Iniciada de ${datosUsuario.name} ${datosUsuario.lastName}`
 }
+async function api (){
+    try {
+        const peticion = await fetch("https://apimocha.com/lucianohtml/users",)
+        const depuracion = await peticion.json()
+        const graduados = document.getElementById("pGraduados")
+        
+        console.log(depuracion)
+        depuracion.forEach((depuracion) => {
+            const li = document.createElement("li")
+            li.innerHTML = `<h5">${depuracion.name} ${depuracion.lastName}</h5>`
+            graduados.append(li)
+        });
+        
+
+        
+        
+        
+    } catch (error) {
+        console.log(error)
+    }    
+}
+api()
 
 function login() {
     const name = document.getElementById("nombre")
@@ -25,11 +53,19 @@ function login() {
     
     if (obj) {
         if (lastName.value === obj.lastName) {
-            span.innerText = `Bienvenido ${obj.name} ${obj.lastName} !`   
+            Swal.fire({
+                title: `Bienvenido ${obj.name} ${obj.lastName} !`,
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            }) 
             return obj
         }
     } else {
-        span.innerText = `Usuario u Contrasena incorrecta !`
+        Swal.fire({
+            title: 'Usuario o contraseña incorrecta',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        })
         return undefined
     }
 }
@@ -38,16 +74,26 @@ function register(){
     const name = document.getElementById("nombre")
     const lastName = document.getElementById("apellido")
     const submit = document.getElementById("submit")
+    const lista =document.getElementById("lista")
+
     let obj = users.find((e) => e.name === name.value)
 
     if (obj) {
-        span.innerText = `El nombre de usuario ya existe`
+        Swal.fire({
+            title: 'Nombre de usuario ya existe',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        })
     } else {
         users.push(new User(name.value, lastName.value))
-        span.innerText = `Usuario ${name.value} creado con exito !`
+        // const Swal = require('sweetalert2')
+        Swal.fire({
+            title: 'Cuenta creada',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+        })
     }
 }
-
 submit.onclick = (e) => {
     e.preventDefault(e)
     boolUserRegister = false
@@ -57,10 +103,9 @@ submit.onclick = (e) => {
         obj = login()
         if(obj){
             localStorage.setItem("login",JSON.stringify(obj))
-        }
+        }``
     }
 }
-
 btnSwitch.onclick = () => {
     let title = document.getElementById("tituloJS");
     let sub = document.getElementById("submit")
@@ -75,3 +120,11 @@ btnSwitch.onclick = () => {
         sub.value = "Registrar"
     }
 }
+
+
+
+
+
+
+
+
